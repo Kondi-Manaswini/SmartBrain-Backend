75474ed2b4d1a23edc74a1739bd820efbb63e678
Manaswini <kmanaswini@iitg.ac.in>
2022-09-23 21:06:38 +0530

    updating database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-objective-77727',
-    user: 'postgres',
-    password: 'PostGreSQL',
-    database: 'smart-brain'
+    host: process.env.DATABSE_URL,
+    ssl:true,
   }
 });
 
