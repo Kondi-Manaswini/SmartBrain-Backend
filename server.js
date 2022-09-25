e2e48f9d52e798c350f1cadaa2cf4ac79ddf1c48
Manaswini <kmanaswini@iitg.ac.in>
2022-09-25 12:34:26 +0530

    second commit

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send(db.users) })
+app.get('/', (req, res) => { "its working" })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
